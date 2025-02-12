cf7d1b21b4740beff84111607202173eafabdf57
inhee-pitt <inhee.lee@pitt.edu>
2025-02-12 10:08:01 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_priority_encoder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,12 +15,28 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    reg [2:0] a;
+  // All output pins must be assigned. If not used, assign to 0
   assign uio_out = 0;
-  assign uio_oe  = 0;
-
+  assign uio_oe  = 0; 
+  assign uo_out[7:3] = 0;
+  always@(ui_in)
+      begin
+          casez(ui_in)
+ 
+              8'b00000001:a = 3'b000;
+              8'b0000001?:a  = 3'b001;
+              8'b000001??:a  = 3'b010;
+              8'b00001???:a = 3'b011;
+              8'b0001????:a  = 3'b100;
+              8'b001?????:a  = 3'b101;
+              8'b01??????:a  = 3'b110;
+              8'b1???????:a = 3'b111;
+ 
+          default:a = 3'bzzz;
+          endcase
+     end
+    assign uo_out[2:0] = a;
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
